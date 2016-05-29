21845b8e6cd061e6a59f9947ac17ffc200300b72
massimobiagioli <biagiolimassimo@gmail.com>
2016-05-29 10:20:07 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,47 @@
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.controlPanel', {
+      url: '/controlPanel',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/controlPanel.html',
+          controller: 'ControlPanelCtrl'
+        }
+      }
+  })
+
+  .state('app.settings', {
+      url: '/settings',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/settings.html',
+          controller: 'SettingsCtrl'
+        }
+      }
+  });
+
+  $urlRouterProvider.otherwise('/app/controlPanel');
+});
---
+++ js/controllers.js
@@ -0,0 +1,13 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  
+})
+
+.controller('ControlPanelCtrl', function($scope) {
+    
+})
+
+.controller('SettingsCtrl', function($scope) {
+    
+});
